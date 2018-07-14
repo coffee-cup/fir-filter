47773b962e788d38e24dedf23046ef7431ad75b6
Jake Runzer <jrunzer@ugls9.ece.uvic.ca>
2018-07-13 17:21:39 -0700

    implement basic fir filter

--- main.c
+++ main.c
@@ -1,15 +1,53 @@
-int fir(int *x, int *coefs, int n) {
-    int sum = 0;
-    for (int i = 0; i < n; ++i) {
-        sum += x[i] * coefs[i];
+#include <stdio.h>
+
+
+void fir(int *x, int *h, int *y, unsigned int num_coefs, const unsigned int N) {
+  int X[8] = {0}; // set working array to 0
+  int yn = 0;
+
+  int i, k;
+
+  // we will have N outputs
+  for (i = 0; i < N; i++) {
+
+    // shift data to right
+    for (k = N - 1; k > 0; k--) {
+      X[k] = X[k - 1];
     }
-    return sum;
+
+    // move input sample to buffer
+    X[0] = x[i];
+
+    // clear output sample
+    yn = 0;
+
+    for (k = 0; k < num_coefs; k++) {
+      yn += h[k] * X[k]; // multiply data on coefficients with accumulation
+    }
+
+    // save yn to output position
+    y[i] = yn;
+  }
+}
+
+void print_output(int *y, unsigned int N) {
+  int i;
+  for (i = 0; i < N; ++i) {
+    printf("%d, ", y[i]);
+  }
+  printf("\n");
 }
 
 int main(void) {
-    int x[5] = {1, 2, 3, 4, 5};
-    int coefs[5] = {1, 2, 3, 4, 5};
-    fir(x, coefs, 5);
+  unsigned int N = 5;
+  unsigned int num_coefs = 4;
 
-    return 0;
+  int x[8] = {2, 4, 6, 4, 2, 0, 0, 0};
+  int h[4] = {3, -1, 2, 1};
+  int y[8] = {0};
+
+  fir(x, h, y, 4, 8);
+  print_output(y, 8);
+
+  return 0;
 }
